7df08d2a759959415c611c8c6c3fc4e7b6889a69
gilgenbergg <lanasvega@gmail.com>
2018-10-10 01:54:14 +0300

    CustomString class implementation in progress

--- chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
@@ -1,6 +1,8 @@
 package edu.technopolis.advjava;
 
 import java.io.Serializable;
+import java.util.*;
+import java.io.*;
 
 /**
  * Реализованная специальным образом строка (аналог {@link java.lang.String}),
@@ -9,35 +11,60 @@ import java.io.Serializable;
  */
 public class CustomString implements CharSequence, Serializable {
     private final char[][] chunks;
+    /*
+     * params according to Java 6 have been added
+     */
+    private final int offset;
+    private final int count;
 
     /*
-     * todo add complimentary fields if required
+     * Group of constructors has been added
      */
+    public CustomString(char[][] chunks) {
+        this(chunks, 0, chunks.length);
+    }
 
+    public CustomString(char[][] chunks, int count) {
+        this(chunks, 0, count);
+    }
 
-    /*
-     * todo add constructor or group of constructors
-     */
-
+    public CustomString(char[][] chunks, int offset, int count) {
+        this.chunks = chunks;
+        this.offset = offset;
+        this.count = count;
+    }
 
     @Override
     public int length() {
-        //todo implement length here
+        return count;
     }
 
     @Override
     public char charAt(int index) {
-        //todo implement charAt here
+        if (index < 0 || index >= count) {
+            throw IndexOutOfBoundsException("Out of bounds!");
+        }
+        return chunks[offset + index];
     }
 
     @Override
     public CharSequence subSequence(int start, int end) {
-        //todo implement subSequence here
+        if (start < 0 || end < 0) || end > length() || start > end) {
+            throw IndexOutOfBoundsException("Out of bounds!");
+        }
+        if ((start == 0) && (end == length())) {
+            return this;
+        }
+        else {
+            int low_bound = offset + start;
+            int up_bound = end - start;
+            return CustomString(chunks, low_bound, up_bound);
+        }
+//        return toString().subSequence(start, end);
     }
 
     @Override
     public String toString() {
-        //todo fold chunks into single char array
-        return new String(/* place folded char array here */);
+        return new String(this.chunks, this.offset, this.count);
     }
 }
