e49cad4c2d636a7c14776fe0918b8b331d3c32f0
Дмитрий <dmitilyin@gmail.com>
2018-10-11 15:44:21 +0300

    доделывать

--- chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
@@ -12,39 +12,108 @@ public class CustomString implements CharSequence, Serializable {
 
     public CustomString(char[][] chunks) {
         this.chunks = chunks;
+        this.length = length();
     }
 
     /*
      * todo add complimentary fields if required
      */
-
+    private int length;
 
     /*
      * todo add constructor or group of constructors
      */
+    public CustomString(char[][] chunks, int offset, int end) {
+        int count = 0;
+
+        this.chunks = chunks;
+    }
+
+    public CustomString(String inputText){
+        int divideCount;
+        divideCount = inputText.length() - inputText.replaceAll("\n","").length();
+        chunks = new char[divideCount][];
+        int offset = 0;
+        for (int i = 0; i < chunks.length; i++) {
+            int indOfDivide = inputText.indexOf('\n',offset);
+            chunks[i] = new char[indOfDivide];
+            chunks[i] = inputText.substring(offset,indOfDivide + 1).toCharArray();
+            offset = indOfDivide + 1;
+        }
+
+        this.length = length();
+    }
 
 
     @Override
     public int length() {
-        //todo implement length here
-        return 0;
+        int resultLength = 0;
+        for (char[] a : chunks) {
+            resultLength += a.length;
+        }
+        return resultLength;
     }
 
     @Override
     public char charAt(int index) {
-        //todo implement charAt here
-        return 0;
+        int k = 0;
+        boolean wasFind = false;
+        char j = 0;
+
+        if (length() < index || index < 0) {
+            throw new StringIndexOutOfBoundsException(index);
+        }
+
+        for (char[] chunk : chunks) {
+            if (!wasFind) {
+                for (char a : chunk) {
+                    j = a;
+                    if (k == index) {
+                        wasFind = true;
+                        break;
+                    } else {
+                        k++;
+                    }
+                }
+            }
+        }
+        return j;
     }
 
     @Override
     public CharSequence subSequence(int start, int end) {
         //todo implement subSequence here
+        int offsetIndex = 0;
+        int endIndex = 0;
+        if (start < end){
+            int summ = 0;
+            int razn = length;
+
+            for (int i = 0; i < chunks.length; i++) {
+                summ += chunks[i].length;
+                if (start < summ){
+                    offsetIndex = i;
+                    break;
+                }
+            }
+
+            for (int i = chunks.length; i > 0; i--) {
+                razn -= chunks[i].length;
+                if (start > razn){
+                    endIndex = i;
+                    break;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public String toString() {
-        //todo fold chunks into single char array
-        return new String(/* place folded char array here */);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char[] a : chunks){
+            stringBuilder.append(a);
+        }
+        return new String(stringBuilder);
     }
 }
--- chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
@@ -1,9 +1,27 @@
 package edu.technopolis.advjava;
 
+import java.util.Scanner;
+
 public class TestCustomString {
 
     public static void main(String[] args) {
-        CustomString customString = new CustomString(new char[][]{{'1', 'R', 'H', '&', '5', '@'}, {}, {'L', '0', 'I'}});
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.print("Введите количество строк в тексте: ");
+        try {
+            int numOfString = Integer.parseInt(scanner.nextLine());
+            for (int i = 0; i < numOfString; i++) {
+                stringBuilder.append(scanner.nextLine());
+                stringBuilder.append('\n');
+            }
+        }catch (Exception e){
+            throw new NumberFormatException();
+        }
+
+        CustomString customString = new CustomString(stringBuilder.toString());
+        System.out.println(customString.charAt(6));
+
+        System.out.println(customString);
     }
 
 }
