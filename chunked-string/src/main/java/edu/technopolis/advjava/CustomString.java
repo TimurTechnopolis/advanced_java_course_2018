045fc26eef6cc26cb502830852ba8c082186e5fd
Дмитрий <dmitilyin@gmail.com>
2018-10-12 15:05:50 +0300

    начал переделывать

--- chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
@@ -13,23 +13,38 @@ public class CustomString implements CharSequence, Serializable {
     /*
      * todo add complimentary fields if required
      */
-
+    private static final int CHUNKS_LENGTH = 10;
+    private int offset;
+    private int flang;
 
     /*
      * todo add constructor or group of constructors
      */
-    public CustomString(char[][] chunks){
+    public CustomString(char[][] chunks) {
         this.chunks = chunks;
+        this.offset = 0;
+        this.flang = CHUNKS_LENGTH - 1;
     }
 
-    CustomString(String inputString){
-        this.chunks = new char[1][1];
+    CustomString(String inputString) {
+        this.offset = 0;
+        chunks = new char[(inputString.length() / 10) + 1][CHUNKS_LENGTH];
+        int iterator = 0;
+        for (int i = 0; i < chunks.length; i++) {
+            for (int j = 0; j < CHUNKS_LENGTH; j++) {
+                if (iterator == inputString.length()) {
+                    break;
+                }
+                chunks[i][j] = inputString.charAt(iterator);
+                this.offset = j;
+                iterator++;
+            }
+        }
     }
 
     @Override
     public int length() {
-        //todo implement length here
-        return 0;
+        return chunks.length * CHUNKS_LENGTH - (offset + 1) - (CHUNKS_LENGTH - flang + 1);
     }
 
     @Override
@@ -47,6 +62,28 @@ public class CustomString implements CharSequence, Serializable {
     @Override
     public String toString() {
         //todo fold chunks into single char array
-        return new String("");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char[] chars : chunks) {
+            for (char a : chars){
+                if (a != '\u0000'){
+                    stringBuilder.append(a);
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    void printChunks() {
+        for (char[] chars : chunks){
+            System.out.println(chars);
+        }
+    }
+
+    public int getFlang() {
+        return flang;
+    }
+
+    public int getOffset() {
+        return offset;
     }
 }
--- chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 public class TestCustomString {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder stringBuilder = new StringBuilder();
-        System.out.print("Введите количество строк в тексте: ");
-        try {
-            int numOfString = Integer.parseInt(scanner.nextLine());
-            for (int i = 0; i < numOfString; i++) {
-                stringBuilder.append(scanner.nextLine());
-                stringBuilder.append('\n');
-            }
-        }catch (Exception e){
-            throw new NumberFormatException();
-        }
+        /*Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();*/
 
-        CustomString customString = new CustomString(stringBuilder.toString());
-        System.out.println(customString.charAt(6));
+        String testString= "Реализованная специальным образом строка (аналог {@link java.lang.String}),\n" +
+                " * хранящий содержимое строки кусочкам (chunks) для лучшего переиспользования памяти при активном\n" +
+                " * создании подстрок.";
 
-        System.out.println(customString.subSequence(5,26));
+        CustomString customString = new CustomString(testString);
+/*        System.out.println(customString.charAt(6));
+
+        System.out.println(customString.subSequence(5,26));*/
+        customString.printChunks();
 
         System.out.println(customString);
+
+        System.out.println("testString length: " + testString.length());
+        System.out.println("customString length: " + customString.length());
     }
 
 }
