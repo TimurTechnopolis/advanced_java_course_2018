928777f68c3dccfab349889505109a3187da5f70
Дмитрий <dmitilyin@gmail.com>
2018-10-12 22:36:54 +0300

    остался subSequence()

--- chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
@@ -1,6 +1,7 @@
 package edu.technopolis.advjava;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Реализованная специальным образом строка (аналог {@link java.lang.String}),
@@ -13,22 +14,39 @@ public class CustomString implements CharSequence, Serializable {
     /*
      * todo add complimentary fields if required
      */
-    private static final int CHUNKS_LENGTH = 10;
+    private final int CHUNKS_LENGTH;
+    private static int defaultChunksLength = 10;
     private int offset;
     private int flang;
 
     /*
      * todo add constructor or group of constructors
      */
-    public CustomString(char[][] chunks) {
+    CustomString(char[][] chunks) {
+        this.CHUNKS_LENGTH = chunks[0].length;
+        for (char[] a : chunks){
+            if (a.length != CHUNKS_LENGTH){
+                throw new IllegalStateException("Чанки не фиксированной длинны");
+            }
+        }
         this.chunks = chunks;
         this.offset = 0;
         this.flang = CHUNKS_LENGTH - 1;
     }
 
-    CustomString(String inputString) {
+    CustomString(String inputSting){
+        this(inputSting, defaultChunksLength);
+    }
+
+    CustomString(String inputString, int chunksLength) {
+        if (chunksLength < 1){
+            throw new IndexOutOfBoundsException();
+        }
+        this.CHUNKS_LENGTH = chunksLength;
         this.offset = 0;
-        chunks = new char[(inputString.length() / CHUNKS_LENGTH) + 1][CHUNKS_LENGTH];
+        int tmp = (inputString.length() / CHUNKS_LENGTH);
+        int size = chunksLength > 1 ? tmp + 1 : tmp;
+        this.chunks = new char[size][CHUNKS_LENGTH];
         int iterator = 0;
         for (int i = 0; i < chunks.length; i++) {
             for (int j = 0; j < CHUNKS_LENGTH; j++) {
@@ -80,9 +98,12 @@ public class CustomString implements CharSequence, Serializable {
     }
 
     void printChunks() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = 1;
         for (char[] chars : chunks) {
-            System.out.println(chars);
+            stringBuilder.append(i++).append(": ").append(Arrays.toString(chars).replaceAll("\n", "\\\\n")).append('\n');
         }
+        System.out.println(stringBuilder.toString());
     }
 
     public int getFlang() {
--- chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/TestCustomString.java
@@ -5,27 +5,60 @@ import java.util.Scanner;
 public class TestCustomString {
 
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        StringBuilder stringBuilder = new StringBuilder();*/
 
-        String testString= "Реализованная специальным образом строка (аналог {@link java.lang.String}),\n" +
+        String testString = "Реализованная специальным образом строка (аналог {@link java.lang.String}),\n" +
                 " * хранящий содержимое строки кусочкам (chunks) для лучшего переиспользования памяти при активном\n" +
                 " * создании подстрок.";
 
-        CustomString customString = new CustomString(testString);
-/*        System.out.println(customString.charAt(6));
+        char[][] testChunks = new char[][]{{'v', 'h', '\n',}, {'\t', 'r', 'y'}, {'y', 'i', 'e'}};
 
-        System.out.println(customString.subSequence(5,26));*/
-        customString.printChunks();
+        CustomString customString1 = new CustomString(testString);
+        CustomString customString2 = new CustomString(testString, 2);
+        CustomString customString3 = new CustomString(testChunks);
 
-        System.out.println(customString);
+        System.out.println("customString1 toString():");
+        System.out.println(customString1);
+        System.out.println("Chunks:");
+        customString1.printChunks();
 
         System.out.println("testString length: " + testString.length());
-        System.out.println("customString length: " + customString.length());
+        System.out.println("customString1 length: " + customString1.length());
 
-        System.out.println(customString.charAt(0));
-        System.out.println(customString.charAt(18));
-        System.out.println(customString.charAt(194));
+        System.out.println(customString1.charAt(0));
+        System.out.println(customString1.charAt(18));
+        System.out.println(customString1.charAt(194));
+
+        System.out.println();
+        System.out.println("==========================================================================");
+        System.out.println();
+
+        System.out.println("customString2 toString():");
+        System.out.println(customString2);
+        System.out.println("Chunks:");
+        customString2.printChunks();
+
+        System.out.println("testString length: " + testString.length());
+        System.out.println("customString2 length: " + customString2.length());
+
+        System.out.println(customString2.charAt(0));
+        System.out.println(customString2.charAt(18));
+        System.out.println(customString2.charAt(194));
+
+        System.out.println();
+        System.out.println("==========================================================================");
+        System.out.println();
+
+        System.out.println("customString3 toString():");
+        System.out.println(customString3);
+        System.out.println("Chunks:");
+        customString3.printChunks();
+
+        System.out.println("customString3 length: " + customString3.length());
+
+        System.out.println(customString3.charAt(0));
+        System.out.println(customString3.charAt(5));
+        System.out.println(customString3.charAt(8));
+
     }
 
 }
