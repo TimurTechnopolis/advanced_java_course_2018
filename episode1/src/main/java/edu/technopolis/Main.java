384172e2186dd31fbc4fdd94bee81cf07415899f
BrainLux <veselblu@yandex.ru>
2018-10-17 20:54:20 +0300

    Final

--- episode1/src/main/java/edu/technopolis/Main.java
+++ episode1/src/main/java/edu/technopolis/Main.java
@@ -6,36 +6,35 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         //Чтение книги с файла
         BufferedReader reader = new BufferedReader(new InputStreamReader(
-                new FileInputStream("voina.txt"), StandardCharsets.UTF_8));
+                new FileInputStream("res/voina.txt"), StandardCharsets.UTF_8));
         StringBuilder s = new StringBuilder();
         while (reader.ready()) {
             s.append(String.valueOf((char) reader.read()));
         }
         //Вывод в качестве экземпляра всей книги "Война и Мир"
-        BufferedWriter writer1 = new BufferedWriter(new FileWriter("out1.txt"));
+        BufferedWriter writer1 = new BufferedWriter(new FileWriter("res/out1.txt"));
         CharSequence string1 = new CustomString(s.toString());
         writer1.write(string1.toString());
         writer1.close();
 
 
         //Вывод в качестве экземпляра второго тома всей книги "Война и Мир"
-        BufferedWriter writer2 = new BufferedWriter(new FileWriter("out2.txt"));
+        BufferedWriter writer2 = new BufferedWriter(new FileWriter("res/out2.txt"));
         CharSequence string2 = string1.subSequence(277602, 516065);
         writer2.write(string2.toString());
         writer2.close();
 
 
         //Вывод в качестве экземпляра предпоследней главы последнего тома всей книги "Война и Мир"
-        BufferedWriter writer3 = new BufferedWriter(new FileWriter("out3.txt"));
+        BufferedWriter writer3 = new BufferedWriter(new FileWriter("res/out3.txt"));
         CharSequence string3 = string1.subSequence(3249716, 3269740);
         writer3.write(string3.toString());
         writer3.close();
 
         //Вывод в качестве экземпляра предпоследней главы второго тома всей книги "Война и Мир"
-        BufferedWriter writer4 = new BufferedWriter(new FileWriter("out4.txt"));
+        BufferedWriter writer4 = new BufferedWriter(new FileWriter("res/out4.txt"));
         CharSequence string4 = string2.subSequence(208784, 221711);
         writer4.write(string4.toString());
         writer4.close();
