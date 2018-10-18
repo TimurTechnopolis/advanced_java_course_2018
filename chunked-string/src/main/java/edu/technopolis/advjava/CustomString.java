4eb1372006462c2422c5eb5677543fe45a25067b
VladislavZasyadko <gilbert.cook111@gmail.com>
2018-10-18 18:14:17 +0300

    customstring test version

--- chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
+++ chunked-string/src/main/java/edu/technopolis/advjava/CustomString.java
@@ -1,6 +1,7 @@
-package edu.technopolis.advjava;
+//package edu.technopolis.advjava;
 
 import java.io.Serializable;
+import java.security.InvalidParameterException;
 
 /**
  * Реализованная специальным образом строка (аналог {@link java.lang.String}),
@@ -10,34 +11,50 @@ import java.io.Serializable;
 public class CustomString implements CharSequence, Serializable {
     private final char[][] chunks;
 
-    /*
-     * todo add complimentary fields if required
-     */
+    private int size;
+    private int offset;
+    private int chunkSize;
+    private int chunkAmount;
+    //private int defaultChunk = 80;
+    //private final int DEFAULT_CHUNK_SIZE = 80;
 
 
-    /*
-     * todo add constructor or group of constructors
-     */
+    public CustomString(String value, int chSize) {
+        if(chSize > 1)
+        this.chunkSize = chSize;
+        else
+            throw new InvalidParameterException("Invalid Chunk Size");
+        this.size = value.length();
+        this.offset = 0;
+        this.chunkAmount = size % chunkSize == 0 ? size/chunkSize : size/chunkSize+1;
+        this.chunks = new char[chunkAmount][chunkSize];
 
+        for (int i = 0; i < chunkAmount; i++)
+            for(int k = 0; k < chunkSize; k++) {
+                if (i*chunkSize+ k < size) {
+                    chunks[i][k] = value.charAt(i * chunkSize + k);
+                }
+            }
 
-    @Override
-    public int length() {
-        //todo implement length here
     }
 
-    @Override
-    public char charAt(int index) {
-        //todo implement charAt here
-    }
+    public CustomString(char[][] arr, int chunkSize, int start, int length) {
+        this.offset = start;
+        this.size = length;
+        this.chunkSize = chunkSize;
+        this.chunkAmount = this.size % this.chunkSize == 0 ? (this.size/ this.chunkSize) : (this.size/ this.chunkSize + 1);
+        this.chunks = new char[chunkAmount][this.chunkSize];
+        int index = offset - 1;
+        for (int i = 0; i < chunkAmount; i++)
 
-    @Override
-    public CharSequence subSequence(int start, int end) {
-        //todo implement subSequence here
-    }
+            for(int k = 0; k < this.chunkSize; k++)
+            {
+                if(i* this.chunkSize + k -(this.offset-1) < this.size) {
+                    chunks[i][k] = arr[index / chunkSize][index % chunkSize];
+                    index++;
+                }
+                else
+                    break;
+            }
 
-    @Override
-    public String toString() {
-        //todo fold chunks into single char array
-        return new String(/* place folded char array here */);
     }
-}
