c12787cef0a2b7c63da9046f0acf58323ef1cfcf
rusyamurzin <scretch235@mail.ru>
2018-11-30 15:14:28 +0300

    Add toString()

--- episode1/src/main/java/edu/technopolis/CustomString.java
+++ episode1/src/main/java/edu/technopolis/CustomString.java
@@ -1,33 +1,40 @@
 package edu.technopolis;
 
-public class CustomString implements CharSequence{
-    int offset;
-    int count;
-    char[][] data;
+import java.io.Serializable;
+
+/**
+ * Реализованная специальным образом строка (аналог {@link java.lang.String}),
+ * хранящий содержимое строки кусочками (chunks) для лучшего переиспользования памяти при активном
+ * создании подстрок.
+ */
+public class CustomString implements CharSequence, Serializable{
+    private int offset;
+    private int count;
+    private char[][] data;
     private int chunkLength = 10;
-    int dataLength;
+    private int dataLength;
 
     public CustomString(String inputString) {
         if(inputString.length() == 0){
             return;
         }
-        this.offset = 0;
-        this.count = 0;
-        this.dataLength = inputString.length();
+        offset = 0;
+        count = 0;
+        dataLength = inputString.length();
         if(inputString.length() % chunkLength == 0){
-            this.data = new char[inputString.length()/chunkLength][chunkLength];
+            data = new char[inputString.length()/chunkLength][chunkLength];
             for(int i = 0; i < inputString.length(); i++){
-                this.data[i/chunkLength][i%chunkLength] = inputString.charAt(i);
+                data[i/chunkLength][i%chunkLength] = inputString.charAt(i);
             }
         }
         else{
-            this.data = new char[inputString.length()/chunkLength+1][];
+            data = new char[inputString.length()/chunkLength+1][];
             for(int i = 0; i < inputString.length()/chunkLength; i++){
-                this.data[i] = new char[chunkLength];
+                data[i] = new char[chunkLength];
             }
-            this.data[inputString.length()/chunkLength] = new char[inputString.length()%chunkLength];
+            data[inputString.length()/chunkLength] = new char[inputString.length()%chunkLength];
             for(int i = 0; i < inputString.length(); i++){
-                this.data[i/chunkLength][i%chunkLength] = inputString.charAt(i);
+                data[i/chunkLength][i%chunkLength] = inputString.charAt(i);
             }
         }
     }
@@ -40,36 +47,38 @@ public class CustomString implements CharSequence{
 
     @Override
     public int length() {
-        if(this.count == 0)
-            return this.dataLength;
+        if(count == 0)
+            return dataLength;
         else
-            return this.count;
+            return count;
     }
 
     @Override
     public char charAt(int index) {
-        return this.data[(this.offset+index)/chunkLength][(this.offset+index)%chunkLength];
+        return data[(offset+index)/chunkLength][(offset+index)%chunkLength];
     }
 
     @Override
     public CustomString subSequence(int start, int end) {
-        return new CustomString(this.offset + start, end - start + 1, this.data, this.dataLength);
+        return new CustomString(offset + start, end - start, data, dataLength);
     }
-    void print(){
+
+    @Override
+    public String toString() {
         StringBuilder outString = new StringBuilder();
         for(int i = 0; i < this.length(); i++){
-            outString.append(this.data[(this.offset + i)/chunkLength][(this.offset + i)%chunkLength]);
+            outString.append(data[(offset + i)/chunkLength][(offset + i)%chunkLength]);
         }
-        System.out.println(outString.toString());
+        return outString.toString();
     }
 
     public static void main(String[] args) {
         CustomString SomeString = new CustomString("I want to break free");
         CustomString SubString = SomeString.subSequence(2, 15);
         CustomString Word = SubString.subSequence(8, 13);
-        SomeString.print();
-        SubString.print();
-        Word.print();
+        System.out.println(SomeString.toString());
+        System.out.println(SubString.toString());
+        System.out.println(Word.toString());
         System.out.println(SomeString.data);
         System.out.println(SubString.data);
         System.out.println(Word.data);
